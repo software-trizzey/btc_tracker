6928499033f95d089eafbd74699b78ae54bd9a60
Tristan Deane <tristandeane93@gmail.com>
2024-11-21 11:17:28 -0700

    Update startup messages in main function for clarity and user guidance

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,14 @@ const THIRTY_MINUTES: Duration = Duration::from_secs(30 * 60);
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    println!("\n{}", "Starting BTC tracker (v1.0.0)...".bold());
+    println!("\n{}", "Welcome to BTC tracker (v1.0.0)!".bold());
+
+    let formatted_time = format!("{} minutes", THIRTY_MINUTES.as_secs() / 60).italic();
+    println!(
+        "\nThis program will fetch the latest BTC quote every {} and store it in an SQLite database.",
+        formatted_time
+    );
+    println!("\nPlease review the README.md to learn how to access the database");
 
     let (tx, mut rx) = watch::channel(());
     let is_running = Arc::new(AtomicBool::new(true));
